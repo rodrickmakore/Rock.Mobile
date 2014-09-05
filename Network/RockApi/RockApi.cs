5f56970581314b662fff2501c7dd97f08823c1d4
jhawkzz <jeredmcferron@gmail.com>
2014-09-05 09:07:08 -0700

    Added methods for saving / loading states from the device. Simplified the mobile user, and added syncing in the case that a change is made while offline.

--- Network/RockApi/RockApi.cs
+++ Network/RockApi/RockApi.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using RestSharp;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace Rock.Mobile
 {
@@ -11,11 +14,11 @@ namespace Rock.Mobile
             const int RequestTimeoutMS = 15000;
 
             const string BaseUrl = "http://rock.ccvonline.com/api";
+            const string NETWORK_OBJECTS_FILENAME = "NetworkObjects.dat";
 
-            System.Net.CookieContainer CookieContainer { get; set; }
+            CookieContainer CookieContainer { get; set; }
 
-            public delegate void LoginResult(System.Net.HttpStatusCode statusCode, string statusDescription);
-
+            public delegate void RequestResult(System.Net.HttpStatusCode statusCode, string statusDescription);
             public delegate void RequestResult<TModel>(System.Net.HttpStatusCode statusCode, string statusDescription, TModel model);
 
             static RockApi _Instance = new RockApi();
@@ -26,7 +29,7 @@ namespace Rock.Mobile
                 CookieContainer = new System.Net.CookieContainer();
             }
 
-            public void Login( string username, string password, LoginResult resultHandler )
+            public void Login( string username, string password, RequestResult resultHandler )
             {
                 RestRequest request = new RestRequest( Method.POST );
                 request.Resource = "Auth/Login";
@@ -35,43 +38,29 @@ namespace Rock.Mobile
                 request.AddParameter( "Password", password );
                 request.AddParameter( "Persisted", true );
 
-                request.Timeout = RequestTimeoutMS;
+                //request.Timeout = RequestTimeoutMS;
+
+                ExecuteAsync( request, resultHandler);
 
                 // make the request
-                RestClient restClient = new RestClient( );
+                /*RestClient restClient = new RestClient( );
                 restClient.BaseUrl = BaseUrl;
                 restClient.CookieContainer = CookieContainer;
 
                 restClient.ExecuteAsync( request, response =>
                     {
-                        if ( response.ErrorException == null )
-                        {
-                            if( response.StatusCode == System.Net.HttpStatusCode.NoContent )
-                            {
-                                // if we received No Content, we're logged in
-                                MobileUser.Instance.Login( username, password );
-                            }
-
-                            Rock.Mobile.Threading.UIThreading.PerformOnUIThread( delegate 
-                                { 
-                                    // notify our caller so they can do whatever it is they wanna do
-                                    resultHandler( response.StatusCode, response.StatusDescription );
-                                });
-                        }
-                        else
-                        {
-                            throw response.ErrorException;
-                        }
-                    });
+                        Rock.Mobile.Threading.UIThreading.PerformOnUIThread( delegate 
+                            { 
+                                // notify our caller so they can do whatever it is they wanna do
+                                resultHandler( response.StatusCode, response.StatusDescription );
+                            });
+                    });*/
             }
 
             public void Logout()
             {
                 // reset our cookies
-                CookieContainer = new System.Net.CookieContainer();
-
-                // logout the user
-                MobileUser.Instance.Logout( );
+                CookieContainer = new CookieContainer();
             }
 
             public void GetProfile( string userName, RequestResult<Rock.Client.Person> resultHandler )
@@ -81,20 +70,38 @@ namespace Rock.Mobile
                 request.Resource = "People/GetByUserName/";
                 request.Resource += string.IsNullOrEmpty( userName ) == true ? MobileUser.Instance.Username : userName;
 
-                ExecuteAsync<Rock.Client.Person>( request, delegate(System.Net.HttpStatusCode code, string desc, Rock.Client.Person model)
-                    {
-                        MobileUser.Instance.Person = model;
+                ExecuteAsync<Rock.Client.Person>( request, resultHandler);
+            }
 
-                        // forward to the result handler
-                        resultHandler( code, desc, model );
+            public void UpdateProfile( Rock.Client.Person person, RequestResult resultHandler )
+            {
+                // request a profile by the username. If no username is specified, we'll use the logged in user's name.
+                RestRequest request = new RestRequest( Method.PUT );
+                request.Resource = "People/";
+                request.Resource += person.Id;
+
+                request.RequestFormat = DataFormat.Json;
+                request.AddBody( person );
+
+                ExecuteAsync( request, resultHandler);
+            }
+
+            /// <summary>
+            /// Wrapper for ExecuteAsync<> that requires no generic Type.
+            /// </summary>
+            /// <param name="request">Request.</param>
+            /// <param name="resultHandler">Result handler.</param>
+            private void ExecuteAsync( RestRequest request, RequestResult resultHandler )
+            {
+                ExecuteAsync<object>( request, delegate(HttpStatusCode statusCode, string statusDescription, object model) 
+                    {
+                        // call the provided handler and drop the dummy object
+                        resultHandler( statusCode, statusDescription );
                     });
             }
 
-            public void ExecuteAsync<TModel>( RestRequest request, RequestResult<TModel> resultHandler ) where TModel : new( )
+            private void ExecuteAsync<TModel>( RestRequest request, RequestResult<TModel> resultHandler ) where TModel : new( )
             {
-                // do not allow api calls if not logged
-                if( MobileUser.Instance.LoggedIn == false ) throw new Exception( "Must log in before making API calls. Call Login()!" );
-
                 RestClient restClient = new RestClient( );
                 restClient.BaseUrl = BaseUrl;
                 restClient.CookieContainer = CookieContainer;
@@ -104,18 +111,73 @@ namespace Rock.Mobile
 
                 restClient.ExecuteAsync<TModel>( request, response => 
                     {
-                        if ( response.ErrorException == null )
-                        {
-                            Rock.Mobile.Threading.UIThreading.PerformOnUIThread( delegate 
-                                { 
-                                    resultHandler( response.StatusCode, response.StatusDescription, response.Data );
-                                });
-                        }
-                        else
-                        {
-                            throw response.ErrorException;
-                        }
+                        // exception or not, notify the caller of the desponse
+                        Rock.Mobile.Threading.UIThreading.PerformOnUIThread( delegate 
+                            { 
+                                resultHandler( response != null ? response.StatusCode : HttpStatusCode.RequestTimeout, 
+                                               response != null ? response.StatusDescription : "Client has no connection.", 
+                                               response != null ? response.Data : new TModel() );
+                            });
                     });
+            }
+
+            public void SaveObjectsToDevice( )
+            {
+                // this will save the current state of all objects to the device,
+                // which is obviously important so we maintain local copies of things
+                // and can access as much as possible without a network connection
+                string filePath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), NETWORK_OBJECTS_FILENAME);
+
+                // open a stream
+                using (StreamWriter writer = new StreamWriter(filePath, false))
+                {
+                    // store our cookies. We cannot serialize the container, so we retrieve and save just the 
+                    // cookies we care about.
+                    CookieCollection cookieCollection = CookieContainer.GetCookies( new Uri( BaseUrl ) );
+                    writer.WriteLine( cookieCollection.Count.ToString() );
+                    for( int i = 0; i < cookieCollection.Count; i++ )
+                    {
+                        string cookieStr = JsonConvert.SerializeObject( cookieCollection[i] );
+                        writer.WriteLine( cookieStr );
+                    }
+
+                    // store the mobile user
+                    writer.WriteLine( MobileUser.Instance.Serialize( ) );
+                }
+            }
+
+            public void LoadObjectsFromDevice( )
+            {
+                // at startup, this should be called to allow current objects to be restored.
+                string filePath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), NETWORK_OBJECTS_FILENAME);
+
+                // if the file exists
+                if(System.IO.File.Exists(filePath) == true)
+                {
+                    // read it
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        // load our cookies
+                        int numCookies = int.Parse( reader.ReadLine() );
+                        for( int i = 0; i < numCookies; i++ )
+                        {
+                            string cookieStr = reader.ReadLine();
+                            Cookie cookie = JsonConvert.DeserializeObject<Cookie>( cookieStr ) as Cookie;
+                            CookieContainer.Add( cookie );
+                        }
+
+                        // load the mobile user
+                        MobileUser.Instance.Deserialize( reader.ReadLine() );
+
+                        //jsonObj = reader.ReadLine();
+                    }
+                }
+            }
+
+            public void SyncWithServer()
+            {
+                // this is a chance for anything unsaved to go ahead and save
+                MobileUser.Instance.SyncDirtyObjects( );
             }
         }
     }
--- Network/RockApi/RockMobileUser.cs
+++ Network/RockApi/RockMobileUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Rock.Client;
+using Newtonsoft.Json;
 
 namespace Rock.Mobile
 {
@@ -17,48 +18,148 @@ namespace Rock.Mobile
             /// Account - Username
             /// </summary>
             /// <value>The username.</value>
-            public string Username { get; private set; }
+            public string Username { get; set; }
 
             /// <summary>
             /// Account - Password
             /// </summary>
             /// <value>The password.</value>
-            public string Password { get; private set; }
+            public string Password { get; set; }
 
             /// <summary>
             /// True when logged in
             /// </summary>
             /// <value><c>true</c> if logged in; otherwise, <c>false</c>.</value>
-            public bool LoggedIn { get; private set; }
+            public bool LoggedIn { get; set; }
 
-            public Person Person { get; set; }
+            /// <summary>
+            /// Person object representing this user's core personal data.
+            /// </summary>
+            /// <value>The person.</value>
+            public Person Person;
+            public string PersonJson { get; set; }
 
             private MobileUser( )
             {
                 Person = new Person();
             }
 
-            public void Login( string username, string password )
+            public string PreferredName( )
             {
-                Username = username;
-                Password = password;
+                if( string.IsNullOrEmpty( Person.NickName ) == false )
+                {
+                    return Person.NickName;
+                }
+                else
+                {
+                    return Person.FirstName;
+                }
+            }
 
-                LoggedIn = true;
+            public void Login( string username, string password, RockApi.RequestResult loginResult )
+            {
+                RockApi.Instance.Login( username, password, delegate(System.Net.HttpStatusCode statusCode, string statusDescription) 
+                    {
+                        // if we received Ok (nocontent), we're logged in.
+                        if( statusCode == System.Net.HttpStatusCode.NoContent )
+                        {
+                            Username = username;
+                            Password = password;
+
+                            LoggedIn = true;
+
+                            // save!
+                            RockApi.Instance.SaveObjectsToDevice( );
+                        }
+
+                        // notify the caller
+                        if( loginResult != null )
+                        { 
+                            loginResult( statusCode, statusDescription );
+                        }
+                    } );
             }
 
             public void Logout( )
             {
-                //Todo: Any wrap up we should do before logging out?
+                // clear the person and take a blank copy
+                Person = new Person();
+                PersonJson = JsonConvert.SerializeObject( Person );
 
                 LoggedIn = false;
 
                 Username = "";
                 Password = "";
+
+                RockApi.Instance.Logout( );
+
+                // save!
+                RockApi.Instance.SaveObjectsToDevice( );
             }
 
+            public void GetProfile( RockApi.RequestResult<Rock.Client.Person> profileResult )
+            {
+                RockApi.Instance.GetProfile( Username, delegate(System.Net.HttpStatusCode statusCode, string statusDescription, Rock.Client.Person model)
+                    {
+                        if( statusCode == System.Net.HttpStatusCode.NoContent || statusCode == System.Net.HttpStatusCode.OK )
+                        {
+                            // on retrieval, convert this version for dirty compares later
+                            Person = model;
+                            PersonJson = JsonConvert.SerializeObject( Person );
 
+                            // save!
+                            RockApi.Instance.SaveObjectsToDevice( );
+                        }
 
+                        // notify the caller
+                        if( profileResult != null )
+                        {
+                            profileResult( statusCode, statusDescription, model );
+                        }
+                    });
+            }
+
+            public void UpdateProfile( RockApi.RequestResult profileResult )
+            {
+                RockApi.Instance.UpdateProfile( Person, delegate(System.Net.HttpStatusCode statusCode, string statusDescription)
+                    {
+                        if( statusCode == System.Net.HttpStatusCode.NoContent )
+                        {
+                            // if successful, update our json so we have a match and don't try to update again later.
+                            PersonJson = JsonConvert.SerializeObject( Person );
+                        }
+
+                        // whether we succeeded in updating with the server or not, save to disk.
+                        RockApi.Instance.SaveObjectsToDevice( );
+
+                        if( profileResult != null )
+                        {
+                            profileResult( statusCode, statusDescription );
+                        }
+                    });
+            }
+
+            public void SyncDirtyObjects( )
+            {
+                // check to see if our person object changed. If our original json
+                // created at a point when we know we were sync'd with the server
+                // no longer matches our object, we should update it.
+                string currPersonJson = JsonConvert.SerializeObject( Person );
+                if( string.Compare( PersonJson, currPersonJson ) != 0 )
+                {
+                    UpdateProfile( null );
+                }
+            }
+
+            public string Serialize( )
+            {
+                return JsonConvert.SerializeObject( this );
+            }
+
+            public void Deserialize( string json )
+            {
+                _Instance = JsonConvert.DeserializeObject<MobileUser>( json ) as MobileUser;
+            }
         }
     }
 }
-
