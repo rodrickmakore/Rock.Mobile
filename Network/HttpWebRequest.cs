114dc39eea5f28da118c5e4696fe898d794252bd
Jered McFerron <jered@Jereds-MacBook-Pro.local>
2014-11-06 11:26:55 -0700

    Reworked HttpWebRequest to use RestSharp and a cleaner interface. Also allows multiple web requests at the same time.

--- Network/HttpWebRequest.cs
+++ Network/HttpWebRequest.cs
@@ -3,11 +3,102 @@ using System.Net;
 using System.IO;
 using System.Collections.Generic;
 using System.Text;
+using RestSharp;
 
 namespace Rock.Mobile
 {
     namespace Network
     {
+        public class HttpRequest
+        {
+            /// <summary>
+            /// The timeout after which the REST call attempt is given up.
+            /// </summary>
+            const int RequestTimeoutMS = 15000;
+
+            /// <summary>
+            /// Request Response delegate that does not require a returned object
+            /// </summary>
+            public delegate void RequestResult(System.Net.HttpStatusCode statusCode, string statusDescription);
+
+            /// <summary>
+            /// Request response delegate that does require a returned object
+            /// </summary>
+            public delegate void RequestResult<TModel>(System.Net.HttpStatusCode statusCode, string statusDescription, TModel model);
+
+            public CookieContainer CookieContainer { get; set; }
+
+            /// <summary>
+            /// Wrapper for ExecuteAsync<> that requires no generic Type.
+            /// </summary>
+            /// <param name="request">Request.</param>
+            /// <param name="resultHandler">Result handler.</param>
+            public void ExecuteAsync( string requestUrl, RestRequest request, RequestResult resultHandler )
+            {
+                ExecuteAsync<object>( requestUrl, request, delegate(HttpStatusCode statusCode, string statusDescription, object model) 
+                    {
+                        // call the provided handler and drop the dummy object
+                        resultHandler( statusCode, statusDescription );
+                    });
+            }
+
+            /// <summary>
+            /// Wrapper for ExecuteAsync<> that returns to the user the raw bytes of the request (useful for image retrieval or any non-object based return type)
+            /// </summary>
+            /// <param name="request">Request.</param>
+            /// <param name="resultHandler">Result handler.</param>
+            public void ExecuteAsync( string requestUrl, RestRequest request, RequestResult<byte[]> resultHandler )
+            {
+                // to give them the raw data, we'll call ExecuteAsync<> and pass in the acutal response object as the type.
+                ExecuteAsync<RestSharp.RestResponse>( requestUrl, request, delegate(HttpStatusCode statusCode, string statusDescription, RestSharp.RestResponse model) 
+                    {
+                        // then, we'll call the result handler and pass the raw bytes.
+                        resultHandler( statusCode, statusDescription, model.RawBytes );
+                    });
+            }
+
+            public void ExecuteAsync<TModel>( string requestUrl, RestRequest request, RequestResult<TModel> resultHandler ) where TModel : new( )
+            {
+                RestClient restClient = new RestClient( );
+                restClient.BaseUrl = requestUrl;
+                restClient.CookieContainer = CookieContainer;
+
+                // set the request format
+                //request.RequestFormat = Format;
+
+                // don't wait longer than 15 seconds
+                request.Timeout = RequestTimeoutMS;
+
+                // if the TModel is RestResponse, that implies they want the actual response, and no parsing.
+                if( typeof( TModel ) == typeof( RestResponse ) )
+                {
+                    restClient.ExecuteAsync( request, response => 
+                        {
+                            // exception or not, notify the caller of the desponse
+                            Rock.Mobile.Threading.UIThreading.PerformOnUIThread( delegate 
+                                { 
+                                    resultHandler( response != null ? response.StatusCode : HttpStatusCode.RequestTimeout, 
+                                        response != null ? response.StatusDescription : "Client has no connection.", 
+                                        (TModel)response );
+                                });
+                        });
+                }
+                else
+                {
+                    restClient.ExecuteAsync<TModel>( request, response => 
+                        {
+                            // exception or not, notify the caller of the desponse
+                            Rock.Mobile.Threading.UIThreading.PerformOnUIThread( delegate 
+                                { 
+                                    resultHandler( response != null ? response.StatusCode : HttpStatusCode.RequestTimeout, 
+                                        response != null ? response.StatusDescription : "Client has no connection.", 
+                                        response != null ? response.Data : new TModel() );
+                                });
+                        });
+                }
+            }
+        }
+
         public class Util
         {
             /// <summary>
@@ -31,86 +122,6 @@ namespace Rock.Mobile
                     }
                 }
                 return false;
-            }
-        }
-
-        //TODO: Either make this not a singleton, or manage a queue ourselves
-
-        public class HttpWebRequest
-        {
-            // singleton pattern
-            static HttpWebRequest _instance;
-
-            static public HttpWebRequest Instance
-            {
-                get
-                {
-                    if( _instance == null )
-                    {
-                        _instance = new HttpWebRequest( );
-                    }
-                    return _instance;
-                }
-            }
-
-            System.Net.WebRequest WebRequest { get; set; }
-
-            // setup a response delegate to provide the web response.
-            public delegate void Callback( Exception e, Dictionary<string, string> responseHeaders, string body );
-
-            private Callback ResponseDelegate { get; set; }
-
-            private HttpWebRequest( )
-            {
-                ResponseDelegate = null;
-            }
-
-            public void MakeAsyncRequest( string url, Callback responseDelegate )
-            {
-                ResponseDelegate = responseDelegate;
-
-                // note - in a better version, we could pass query or body args
-                // instead of doing it all in the caller.
-                WebRequest = System.Net.WebRequest.Create( url );
-                WebRequest.Method = "GET";
-                WebRequest.Timeout = 10000;
-
-                WebRequest.BeginGetResponse( new AsyncCallback( FinishWebRequest ), null );
-            }
-
-            void FinishWebRequest( IAsyncResult result )
-            {
-                Exception ex = null;
-                Dictionary<string, string> responseHeaders = null;
-                string responseBody = null;
-
-                try
-                {
-                    // get the response
-                    WebResponse response = WebRequest.EndGetResponse( result );
-
-                    // parse the headers
-                    responseHeaders = new Dictionary<string, string>( );
-                    for( int i = 0; i < response.Headers.Count; i++ )
-                    {
-                        responseHeaders.Add( response.Headers.GetKey( i ), response.Headers.Get( i ) );
-                    }
-
-                    // get the body
-                    Stream dataStream = response.GetResponseStream( );
-                    StreamReader reader = new StreamReader( dataStream );
-                    responseBody = reader.ReadToEnd( );
-                } 
-                catch( Exception e )
-                {
-                    ex = e;
-                }
-
-                // and notify the original caller
-                Callback responder = ResponseDelegate;
-                ResponseDelegate = null;
-
-                responder( ex, responseHeaders, responseBody );
             }
         }
     }
